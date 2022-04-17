275f8d119b297236bf409e6e02b3d40a228f749f
Vicente Bruno Nonato Paranhos <vicentebrunonp@outlook.com>
2022-04-17 11:15:18 -0300

    Init

---
+++ SGBenefic.API/Controllers/AlunoController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SGBenefic.API.Data;
+using SGBenefic.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SGBenefic.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public AlunoController(DataContext context)
+        {
+            _context = context;
+        }
+        
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Alunos);
+        }
+
+        [HttpGet("GetById/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(x => x.Id == id);
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+
+            return Ok(aluno);
+        }
+
+        [HttpGet("GetByName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(x =>
+                x.Nome.ToLower().Contains(nome.ToLower())
+                && x.SobreNome.ToLower().Contains(sobrenome.ToLower())
+             );
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Aluno aluno)
+        {
+            _context.Add(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Aluno aluno)
+        {
+            var _aluno = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (_aluno == null) return BadRequest("Aluno não encontrado");
+
+            _context.Update(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, [FromBody] Aluno aluno)
+        {
+            var _aluno = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (_aluno == null) return BadRequest("Aluno não encontrado");
+
+            _context.Update(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+
+            _context.Remove(aluno);
+            return Ok();
+        }
+    }
+}
---
+++ SGBenefic.API/Models/Beneficit.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SGBenefic.API.Models
+{
+    public class Beneficit
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [JsonPropertyName("CPF_CNPJ")]
+        public string CpfCnpj { get; set; }
+
+        [JsonPropertyName("RAZAO_SOCIAL")]
+        public string RazaoSocial { get; set; }
+
+        [JsonPropertyName("NIVEL")]
+        public string Nivel { get; set; }
+
+        [JsonPropertyName("CODIGO_BENEFICIO")]
+        [Required]
+        public string CodigoBeneficio { get; set; }
+
+        [JsonPropertyName("BENEFICIO")]
+        [Required]
+        public string Beneficio { get; set; }
+
+        [JsonPropertyName("ORG_COD")]
+        [Required]
+        public string OrgCod { get; set; }
+
+        [JsonPropertyName("DATA_CONCESSAO_BENEFICIO")]
+        [Required]
+        public DateTime DataConcessaoBeneficio { get; set; }
+
+        [JsonPropertyName("DATA_VALIDADE_BENEFICIO")]
+        public DateTime? DataValidadeBeneficio { get; set; } = null;
+
+        [JsonPropertyName("DATA_UTILIZACAO_BENEFICIO")]
+        public DateTime? DataUtilizacaoBeneficio { get; set; } = null;
+
+        [JsonPropertyName("BENEFICIO_UTILIZADO")]
+        [Required]
+        public int BeneficioUtilizado { get; set; } = 0;
+
+        [JsonPropertyName("COMENTARIO")]
+        public string Comentario { get; set; }
+    }
+}
